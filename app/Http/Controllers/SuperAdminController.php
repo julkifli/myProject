6b2025a830e35d5288802a2bc29682128cd8e3db
syclops <julkifli@gmail.com>
2020-01-13 10:27:45 +0800

    First Commit

---
+++ app/Http/Controllers/SuperAdminController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SuperAdminController extends Controller
+{
+
+            //Index method for SuperAdmin Controller
+            public function index()
+            {
+                return view('superadmin.home');
+            }
+
+            public function __construct()
+            {
+                $this->middleware('auth');
+                $this->middleware('role:ROLE_SUPERADMIN');
+            }
+
+}
